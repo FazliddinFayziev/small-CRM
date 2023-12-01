3b8b7d2b78e20d3bf498e104bf3d9fd06efc6351
FazliddinFayziev <fazrez4515@gmail.com>
2023-12-01 16:50:25 +0800

    Customer page is done

--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse 
 from .models import Product, Order, Customer
 
-
+# ===> Home
 def home(request):
     orders = Order.objects.all()
     customers = Customer.objects.all()
@@ -22,10 +22,18 @@ def home(request):
     return render(request, 'accounts/dashboard.html', context)
     
     
+# ===> Products
 def products(request):
     products = Product.objects.all()
     return render(request, 'accounts/products.html', {'products': products})
 
 
-def customer(request):
-    return render(request, 'accounts/customer.html')
+# ===> Customer
+def customer(request, pk):
+    customer = Customer.objects.get(id=pk)
+     
+    orders = customer.order_set.all()
+    order_count = orders.count()
+    
+    context = {'customer': customer, 'orders': orders, 'order_count': order_count}
+    return render(request, 'accounts/customer.html', context)
